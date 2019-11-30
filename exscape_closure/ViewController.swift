640ae00aa1935fe22924d0e53e5026977752af81
Sooyong  Kim <30457077+dev-wd@users.noreply.github.com>
2019-11-30 14:26:10 +0900

    :sparkles: ExscapeClosure Example

---
+++ exscape_closure/ViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Declare closure for case 2.
+    
+    let getColorClosure : (String) -> Void = {
+        color in
+        print(color)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Implement case 1
+        
+        getFruitColorwithTrailingClosure("banana") {
+            color in
+            
+            print(color)
+        }
+        
+        // Implement case 2
+        
+        getFruitColorwithClosure("melon",completion: getColorClosure)
+    }
+    
+    
+    // case 1 : Completion Handle with TrailingClosure
+    
+    func getFruitColorwithTrailingClosure(_ fruitName : String, completion: @escaping (String)-> Void) {
+        
+        var color = "Not yet"
+        
+        if fruitName == "banana" {
+            color = "yellow"
+        }
+        
+        if fruitName == "apple" {
+            color = "red"
+        }
+        
+        if fruitName == "melon" {
+            color = "green"
+        }
+        
+        completion(color)
+    }
+    
+    // case 2 : Completion Handle with DeclaredClosure
+    
+    func getFruitColorwithClosure(_ fruitName : String, completion: @escaping (String)-> Void) {
+        var color = "Not yet"
+        
+        if fruitName == "banana" {
+            color = "yellow"
+        }
+        
+        if fruitName == "apple" {
+            color = "red"
+        }
+        
+        if fruitName == "melon" {
+            color = "green"
+        }
+        
+        completion(color)
+        
+    }
+
+}
