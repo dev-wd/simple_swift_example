55c5801f93479b3e03a341e5bcb3c9d11e48363e
Sooyong  Kim <30457077+dev-wd@users.noreply.github.com>
2020-01-10 16:20:02 +0900

    Create AppDelegate.swift

---
+++ Coordinator/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var coordinator: MainCoordinator?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navController = UINavigationController()
+        navController.navigationBar.isHidden = true
+        coordinator = MainCoordinator(navigationController: navController)
+        coordinator?.start()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
